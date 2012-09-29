f1a1a17df1c339c0eb49647b78c8f4ef9178f153
Ryan Imel <ryan@aspiringindie.com>
2012-09-28 20:04:05 -0400

    Initial plugin plus content types, removing what we don't need. Same old song and dance.

---
+++ founders-list-plugin.php
@@ -0,0 +1,107 @@
+<?php
+/*
+Plugin Name: Founders List Plugin
+Plugin URI: 
+Description: 
+Author: Ryan Imel
+Version: 0.1
+Author URI: http://wpcandy.com
+*/
+
+
+/**
+ * Add our custom content.
+ */
+add_action( 'init', 'fl_custom_content_types' );
+
+function fl_custom_content_types() {
+	$person_labels = array(
+		'name' => _x('People', 'post type general name', 'founders_list_plugin'),
+		'singular_name' => _x('Person', 'post type singular name', 'founders_list_plugin'),
+		'add_new' => _x('Add New', 'book', 'founders_list_plugin'),
+		'add_new_item' => __('Add New Person', 'founders_list_plugin'),
+		'edit_item' => __('Edit Person', 'founders_list_plugin'),
+		'new_item' => __('New Person', 'founders_list_plugin'),
+		'all_items' => __('All People', 'founders_list_plugin'),
+		'view_item' => __('View Person', 'founders_list_plugin'),
+		'search_items' => __('Search People', 'founders_list_plugin'),
+		'not_found' =>  __('No people found', 'founders_list_plugin'),
+		'not_found_in_trash' => __('No people found in Trash', 'founders_list_plugin'), 
+		'parent_item_colon' => '',
+		'menu_name' => __('People', 'founders_list_plugin')
+	);
+	$person_args = array(
+		'labels' => $person_labels,
+		'public' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true, 
+		'show_in_menu' => true, 
+		'query_var' => true,
+		'rewrite' => array( 'slug' => _x( 'person', 'URL slug', 'founders_list_plugin' ) ),
+		'capability_type' => 'post',
+		'has_archive' => true, 
+		'hierarchical' => false,
+		'menu_position' => null,
+		'supports' => array( 'title', 'editor', 'thumbnail', 'excerpt' )
+	);
+	register_post_type( 'fl_person', $person_args );
+	
+	$labels = array(
+		'name' => _x( 'Trades', 'taxonomy general name', 'founders_list_plugin' ),
+		'singular_name' => _x( 'Trade', 'taxonomy singular name', 'founders_list_plugin' ),
+		'search_items' =>  __( 'Search Trades', 'founders_list_plugin' ),
+		'all_items' => __( 'All Trades', 'founders_list_plugin' ),
+		'parent_item' => __( 'Parent Trade', 'founders_list_plugin' ),
+		'parent_item_colon' => __( 'Parent Trade:', 'founders_list_plugin' ),
+		'edit_item' => __( 'Edit Trade', 'founders_list_plugin' ), 
+		'update_item' => __( 'Update Trade', 'founders_list_plugin' ),
+		'add_new_item' => __( 'Add New Trade', 'founders_list_plugin' ),
+		'new_item_name' => __( 'New Trade Name', 'founders_list_plugin' ),
+		'menu_name' => __( 'Trades', 'founders_list_plugin' ),
+	); 	
+	
+	register_taxonomy(
+		'fl_trade',
+		array( 'fl_person' ), 
+		array(
+			'hierarchical' => true,
+			'labels' => $labels,
+			'show_ui' => true,
+			'query_var' => true,
+			'rewrite' => array( 'slug' => 'trade' ),
+		)
+	);
+}
+
+
+/**
+ * Remove what we don't need.
+ */
+add_action( 'admin_menu', 'fl_remove_menu_items' );
+
+function fl_remove_menu_items() {
+	remove_menu_page( 'edit.php' );
+	remove_menu_page( 'upload.php' );
+	remove_menu_page( 'edit.php?post_type=page' );
+	remove_menu_page( 'edit-comments.php' );
+}
+
+
+/**
+ * Remove dashboard widgets we don't need.
+ */
+add_action( 'wp_dashboard_setup', 'fl_remove_dashboard_widgets' );
+
+function fl_remove_dashboard_widgets() {
+	// Main widgets
+	remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+	remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+	remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+	remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+	
+	// Side widgets
+	remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+	remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+	remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+	remove_meta_box( 'dashboard_secondary', 'dashboard', 'side' );
+}
